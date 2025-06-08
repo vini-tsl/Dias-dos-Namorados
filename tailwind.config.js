49e4f964aeb7b1cd726ea2bc9f534705b7f3a20c
vini-tsl <viniciusteixeira022@gmail.com>
2025-06-08 20:54:14 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+    "./static/**/*.{js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
